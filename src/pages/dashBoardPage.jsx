8f4f52bd0ae68ae0d832b19865fe99bda3cb681c
Hyunjung Cho <hyunjungcho@Hyunjungui-MacBookPro.local>
2023-05-15 13:36:00 +0900

    Add components/kakaoLogin

--- src/pages/dashBoardPage.jsx
+++ src/pages/dashBoardPage.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-const DashboardPage = () => {
-  const { isLoading, isError, data } = useQuery("allProject", allProject);
-  console.log(data);
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error: {Error.message}</div>;
+// const DashboardPage = () => {
+//   const { isLoading, isError, data } = useQuery("allProject", allProject);
+//   console.log(data);
+//   if (isLoading) return <div>Loading...</div>;
+//   if (isError) return <div>Error: {Error.message}</div>;
 
-  // useEffect(() => {
-  // 서버에 데이터 요청
-  // 현재 컴포넌트에서 필요한 데이터를 요청함
+//   // useEffect(() => {
+//   // 서버에 데이터 요청
+//   // 현재 컴포넌트에서 필요한 데이터를 요청함
 
-  // const detailOpen = (sprintId) => {
-  //   if (!Cookies.get('token')) {
-  //     alert('로그인이 필요합니다.')
-  //     return;
-  //   }
-  // }, []);
+//   // const detailOpen = (sprintId) => {
+//   //   if (!Cookies.get('token')) {
+//   //     alert('로그인이 필요합니다.')
+//   //     return;
+//   //   }
+//   // }, []);
 
-  return (
-    <div>
-      <h1>DashboardPage</h1>
-      <ul>
-        {projects.map((project) => (
-          <li key={project.id}>{project.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>DashboardPage</h1>
+//       <ul>
+//         {projects.map((project) => (
+//           <li key={project.id}>{project.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default DashboardPage;
+// export default DashboardPage;
